46e3cf4a1504982880dc977e5498e9f78ac0ea1f
Guillaume Deconinck <gui.deconinck@gmail.com>
2022-07-10 20:02:10 +0200

    feat: add dummy ping route

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
@@ -10,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
         App::new()
             .wrap(TracingLogger::default())
             .app_data(db_pool.clone())
+            .route("/ping", web::get().to(HttpResponse::Ok))
     })
     .listen(listener)?
     .run();
